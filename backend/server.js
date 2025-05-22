82712fe45098c20970b2253da2af48b72280c198
Tot <tot74738@gmail.com>
2025-05-22 02:16:15 -0500

    Primera versión de pruba de votaciones-app

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ app.use(express.json());
 
 // Conexión a la base de datos
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'votaciones'
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 // Login
